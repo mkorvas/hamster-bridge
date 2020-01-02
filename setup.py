0cbf1a276141ee260d87e3893cd1493c1576f455
Matěj Korvas <matej.korvas@cerence.com>
2020-01-02 13:36:32 +0100

    List missing dependencies in setup.py
    Closes kraiz/hamster-bridge#29.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='hamster-bridge',
     description='let your hamster log your work to your favorite bugtracker',
-    version='0.7.0',
+    version='0.8.0',
     author='Lars Kreisz',
     author_email='lars.kreisz@gmail.com',
     license='MIT',
@@ -16,5 +16,9 @@ setup(
     packages=['hamster_bridge', 'hamster_bridge.listeners'],
     entry_points={'console_scripts': ['hamster-bridge = hamster_bridge:main']},
     long_description=open('README.rst').read(),
-    install_requires=['jira>=0.41']
+    install_requires=[
+        'configparser>=4.0.0',
+        'jira>=0.41',
+        'python-dateutil>=2.8.0',
+    ]
 )
